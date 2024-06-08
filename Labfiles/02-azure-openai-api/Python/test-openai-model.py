584f2a96b30c0c35be5fe06e1f6e1263f1a9efbc
Pascal Anene <pascal.anene@husqvarnagroup.com>
2024-06-08 09:15:01 +0200

    LB-02 add code for Lab 02

--- Labfiles/02-azure-openai-api/Python/test-openai-model.py
+++ Labfiles/02-azure-openai-api/Python/test-openai-model.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 
 # Add Azure OpenAI package
+from openai import AzureOpenAI
 
 
 def main(): 
@@ -15,8 +16,22 @@ def main():
         azure_oai_deployment = os.getenv("AZURE_OAI_DEPLOYMENT")
         
         # Initialize the Azure OpenAI client...
-        
+        client = AzureOpenAI(
+            azure_endpoint = azure_oai_endpoint, 
+            api_key=azure_oai_key,  
+            api_version="2024-02-15-preview"
+            )
+    
+        # Create a system message
+        system_message = """I am a hiking enthusiast named Forest who helps people discover hikes in their area. 
+            If no area is specified, I will default to near Rainier National Park. 
+            I will then provide three suggestions for nearby hikes that vary in length. 
+            I will also share an interesting fact about the local nature on the hikes when making a recommendation.
+            """
 
+        # Messages array allows us to provide conversation history to the model. 
+        messages_array = [{"role": "system", "content": system_message}]
+        
 
         while True:
             # Get input text
@@ -29,9 +44,42 @@ def main():
 
             print("\nSending request for summary to Azure OpenAI endpoint...\n\n")
             
-            # Add code to send request...
+            # PART A: Add code to send request. WITHOUT CONVERSATION HISTORY
+
+            # Send request to Azure OpenAI model
+            # response = client.chat.completions.create(
+            #     model=azure_oai_deployment,
+            #     temperature=0.7,
+            #     max_tokens=400,
+            #     messages=[
+            #         {"role": "system", "content": system_message},
+            #         {"role": "user", "content": input_text}
+            #     ]
+            # )
+            # generated_text = response.choices[0].message.content
+
+            # # Print the response
+            # print("Response: " + generated_text + "\n")
+
             
             
+            # PART B: Add code to send request. WITH CONVERSATION HISTORY
+            
+            # Send request to Azure OpenAI model
+            messages_array.append({"role": "user", "content": input_text})
+
+            response = client.chat.completions.create(
+                model=azure_oai_deployment,
+                temperature=0.7,
+                max_tokens=1200,
+                messages=messages_array
+            )
+            generated_text = response.choices[0].message.content
+            # Add generated text to messages array
+            messages_array.append({"role": "assistant", "content": generated_text})
+
+            # Print generated text
+            print("Summary: " + generated_text + "\n")
             
 
     except Exception as ex:
